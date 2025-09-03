42c7f459eeee6f9106db19f1b5efe37e764d0aab
Gray Mitchell <gray@igriss.com>
2025-09-03 11:00:34 -0500

    Update App.tsx

--- frontend_demo/src/App.tsx
+++ frontend_demo/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import RetellCallStudio from "./components/RetellCallStudio";
+import RetellCallStudio from "./my-folder/RetellCallStudio.tsx";
 import "./App.css";
 
 export default function App() {
